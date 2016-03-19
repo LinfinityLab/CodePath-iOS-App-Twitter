81de6b67f9f90ec1f71fb577458a1aa52ecaad0d
Weifan Lin <wlin15@citymail.cuny.edu>
2016-03-19 15:50:06 -0400

    added User & Tweet model

--- Twitter/Tweet.swift
+++ Twitter/Tweet.swift
@@ -10,4 +10,35 @@ import UIKit
 
 class Tweet: NSObject {
 
+    var text: NSString?
+    var timestamp: NSDate?
+    var retweetCount: Int = 0
+    var favoritesCount: Int = 0
+    
+    init(dictionary: NSDictionary) {
+        
+        text = dictionary["text"] as? String
+        
+        retweetCount = (dictionary["retweet_count"] as? Int) ?? 0
+        favoritesCount = (dictionary["favourites_count"] as? Int) ?? 0
+        
+        let timestampString = dictionary["created_at"] as? String
+        
+        if let timestampString = timestampString {
+            let formatter = NSDateFormatter()
+            formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
+            timestamp = formatter.dateFromString(timestampString)
+        }
+    }
+    
+    class func tweetWithArray(dictionaries: [NSDictionary]) -> [Tweet] {
+        var tweets = [Tweet]()
+        for dictionary  in dictionaries {
+            let tweet = Tweet(dictionary: dictionary)
+            tweets.append(tweet)
+        }
+        
+        return tweets
+    }
+    
 }
